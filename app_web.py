3eca09cb9ca52f183ac8fe564886fcd8da5e2a03
robrvs <39736736+robrvs@users.noreply.github.com>
2026-08-05 11:30:19 -0400

    Initial commit

---
+++ app_web.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app_web = Flask(__name__)
+
+@app_web.route("/")
+def inicio():
+    return "Página Inicial"
+
+@app_web.route("/ola")
+def raiz():
+    return "Olá, turma!"
+
+if __name__ == "__main__":
+    app_web.run(host="127.0.0.1", port=5000, debug=True)
+    # app_web.run(debug=True)
